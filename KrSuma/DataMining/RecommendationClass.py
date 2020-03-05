837f620ff4fa23b0372b277921b15ccc21157ea7
KrSuma <jaesun90@gmail.com>
2020-03-05 16:57:55 +0900

    minor updates

--- KrSuma/DataMining/RecommendationClass.py
+++ KrSuma/DataMining/RecommendationClass.py
@@ -1,15 +1,28 @@
 import codecs
 from math import sqrt
 
-users = {"Angelica": {"Blues Traveler": 3.5, "Broken Bells": 2.0, "Norah Jones": 4.5, "Phoenix": 5.0, "Slightly Stoopid": 1.5, "The Strokes": 2.5, "Vampire Weekend": 2.0},
-         "Bill":{"Blues Traveler": 2.0, "Broken Bells": 3.5, "Deadmau5": 4.0, "Phoenix": 2.0, "Slightly Stoopid": 3.5, "Vampire Weekend": 3.0},
-         "Chan": {"Blues Traveler": 5.0, "Broken Bells": 1.0, "Deadmau5": 1.0, "Norah Jones": 3.0, "Phoenix": 5, "Slightly Stoopid": 1.0},
-         "Dan": {"Blues Traveler": 3.0, "Broken Bells": 4.0, "Deadmau5": 4.5, "Phoenix": 3.0, "Slightly Stoopid": 4.5, "The Strokes": 4.0, "Vampire Weekend": 2.0},
-         "Hailey": {"Broken Bells": 4.0, "Deadmau5": 1.0, "Norah Jones": 4.0, "The Strokes": 4.0, "Vampire Weekend": 1.0},
-         "Jordyn":  {"Broken Bells": 4.5, "Deadmau5": 4.0, "Norah Jones": 5.0, "Phoenix": 5.0, "Slightly Stoopid": 4.5, "The Strokes": 4.0, "Vampire Weekend": 4.0},
-         "Sam": {"Blues Traveler": 5.0, "Broken Bells": 2.0, "Norah Jones": 3.0, "Phoenix": 5.0, "Slightly Stoopid": 4.0, "The Strokes": 5.0},
-         "Veronica": {"Blues Traveler": 3.0, "Norah Jones": 5.0, "Phoenix": 4.0, "Slightly Stoopid": 2.5, "The Strokes": 3.0}
-        }
+'''
+program based on the collaborative filtering research - 
+equations and algorithms based on the notes.
+'''
+
+users = {"Angelica": {"Blues Traveler": 3.5, "Broken Bells": 2.0, "Norah Jones": 4.5, "Phoenix": 5.0,
+                      "Slightly Stoopid": 1.5, "The Strokes": 2.5, "Vampire Weekend": 2.0},
+         "Bill": {"Blues Traveler": 2.0, "Broken Bells": 3.5, "Deadmau5": 4.0, "Phoenix": 2.0, "Slightly Stoopid": 3.5,
+                  "Vampire Weekend": 3.0},
+         "Chan": {"Blues Traveler": 5.0, "Broken Bells": 1.0, "Deadmau5": 1.0, "Norah Jones": 3.0, "Phoenix": 5,
+                  "Slightly Stoopid": 1.0},
+         "Dan": {"Blues Traveler": 3.0, "Broken Bells": 4.0, "Deadmau5": 4.5, "Phoenix": 3.0, "Slightly Stoopid": 4.5,
+                 "The Strokes": 4.0, "Vampire Weekend": 2.0},
+         "Hailey": {"Broken Bells": 4.0, "Deadmau5": 1.0, "Norah Jones": 4.0, "The Strokes": 4.0,
+                    "Vampire Weekend": 1.0},
+         "Jordyn": {"Broken Bells": 4.5, "Deadmau5": 4.0, "Norah Jones": 5.0, "Phoenix": 5.0, "Slightly Stoopid": 4.5,
+                    "The Strokes": 4.0, "Vampire Weekend": 4.0},
+         "Sam": {"Blues Traveler": 5.0, "Broken Bells": 2.0, "Norah Jones": 3.0, "Phoenix": 5.0,
+                 "Slightly Stoopid": 4.0, "The Strokes": 5.0},
+         "Veronica": {"Blues Traveler": 3.0, "Norah Jones": 5.0, "Phoenix": 4.0, "Slightly Stoopid": 2.5,
+                      "The Strokes": 3.0}
+         }
 
 
 class recommender:
@@ -30,18 +43,18 @@ class recommender:
                 return self.productid2name[id]
             else:
                 return id
+
         # return n top ratings for user with id
         def userRatings(self, id, n):
             print("Ratings for " + self.userid2name[id])
             ratings = self.data[id]
-            print (len(ratings))
+            print(len(ratings))
             ratings = list(ratings.items())
-            ratings = [(self.converProductID2name(k), v for (k, v) in ratings]
-            ratings.sort(key=lambda artistTuple: artistTuple[1], reverse = True)
+            ratings = [(self.converProductID2name(k), v) for (k, v) in ratings]
+            ratings.sort(key=lambda artistTuple: artistTuple[1], reverse=True)
             ratings = ratings[:n]
             for rating in ratings:
                 print("%s\t%i" % (rating[0], rating[1]))
-
 
         def pearsonCorrelation(self, rating1, rating2):
             numerator = 0
--- KrSuma/DataMining/StudyLog.md
+++ KrSuma/DataMining/StudyLog.md
@@ -12,4 +12,14 @@ Log: 3/4/2020
 
 1. note complete on collaborative filtering
 2. received the dataset sample from the company server
-3. 
+3. started on the prototype for machine learning using the company dataset sample
+
+LogL 3/5/2020
+
+1. started working on recommendation class program based on collaborative filtering
+2. made some progress on the machine learning prototype
+
+to-do : 
+1. need bigger dataset sample - dataset is too small for any training 
+2. need a general purpose and direction for the machine learning project
+3. need more revision on data cleaning and data analysis
