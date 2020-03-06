1e39734c7a2b184fdb341f388c8a98cb0a24f33a
KrSuma <jaesun90@gmail.com>
2020-03-06 13:32:45 +0900

    updated recommender class

--- KrSuma/DataMining/RecommendationClass.py
+++ KrSuma/DataMining/RecommendationClass.py
@@ -24,67 +24,162 @@ users = {"Angelica": {"Blues Traveler": 3.5, "Broken Bells": 2.0, "Norah Jones":
                       "The Strokes": 3.0}
          }
 
-
 class recommender:
+
     def __init__(self, data, k=1, metric='pearson', n=5):
         self.k = k
         self.n = n
         self.username2id = {}
         self.userid2name = {}
         self.productid2name = {}
+
+        self.frequencies = {}
+        self.deviations = {}
+
         self.metric = metric
+        if self.metric == 'pearson':
+            self.fn = self.pearson
 
         if type(data).__name__ == 'dict':
             self.data = data
 
-        # function to convert product id number into product name
-        def convertProductID2Name(self, id):
-            if id in self.productid2name:
-                return self.productid2name[id]
-            else:
-                return id
+    def convertProductID2name(self, id):
+        if id in self.productid2name:
+            return self.productid2name[id]
+        else:
+            return id
 
-        # return n top ratings for user with id
-        def userRatings(self, id, n):
-            print("Ratings for " + self.userid2name[id])
-            ratings = self.data[id]
-            print(len(ratings))
-            ratings = list(ratings.items())
-            ratings = [(self.converProductID2name(k), v) for (k, v) in ratings]
-            ratings.sort(key=lambda artistTuple: artistTuple[1], reverse=True)
-            ratings = ratings[:n]
-            for rating in ratings:
-                print("%s\t%i" % (rating[0], rating[1]))
+    def userRatings(self, id, n):
+        # print("Ratings for " + self.userid2name[id])
+        # ratings = self.data[id]
+        # print(len(ratings))
+        # ratings = list(ratings.items())[:n]
+        # ratings = [(self.convertProductID2name(k), v)
+        #            for (k, v) in ratings]
+        # ratings.sort(key=lambda artistTuple: artistTuple[1],
+        #              reverse=True)
+        # for rating in ratings:
+        #     print("%s\t%i" % (rating[0], rating[1]))
+        pass
 
-        def pearsonCorrelation(self, rating1, rating2):
-            numerator = 0
-            x = 0
-            y = 0
-            x_sqrd = 0
-            y_sqrd = 0
-            count = 0
-            for key in rating1:
-                if key in rating2:
-                    numerator += (rating1[key] * rating2[key])
-                    x += rating1[key]
-                    y += rating2[key]
-                    x_sqrd += pow(rating1[key], 2)
-                    y_sqrd += pow(rating2[key], 2)
-                    count += 1
-            if count == 0:
-                return 0
-            numerator = numerator - (x * y) / count
-            denominator1 = pow((x_sqrd - (pow(x, 2)) / count), 1 / 2)
-            denominator2 = pow((y_sqrd - (pow(y, 2)) / count), 1 / 2)
-            return numerator / (denominator1 * denominator2)
+    def showUserTopItems(self, user, n):
+        items = list(self.data[user].items())
+        items.sort(key=lambda itemTuple: itemTuple[1], reverse=True)
+        for i in range(n):
+            print("%s\t%i" % (self.convertProductID2name(items[i][0]),
+                              items[i][1]))
 
-        # creating a sorted list of users based on their distance to the username
-        def computeNearestNeighbor(self, username):
-            distances = []
-            for instance in self.data:
-                if instance != username:
-                    distance = self.fn(self.data[username], self.data[instance])
-            distances.append((instance, distance))
-            # sort based on distance -- closest first
-            distances.sort(key=lambda artistTuple: artistTuple[1], reverse=True)
-            return distances
+    def loadCSV(self, path=''):
+        pass
+
+    def computeDeviations(self):
+        for ratings in self.data.values():
+            for (item, rating) in ratings.items():
+                self.frequencies.setdefault(item, {})
+                self.deviations.setdefault(item, {})
+                for (item2, rating2) in ratings.items():
+                    if item != item2:
+                        self.frequencies[item].setdefault(item2, 0)
+                        self.deviations[item].setdefault(item2, 0.0)
+                        self.frequencies[item][item2] += 1
+                        self.deviations[item][item2] += rating - rating2
+
+        for (item, ratings) in self.deviations.items():
+            for item2 in ratings:
+                ratings[item2] /= self.frequencies[item][item2]
+
+    def slopeOneRecommendations(self, userRatings):
+        recommendations = {}
+        frequencies = {}
+        # for every item and rating in the user's recommendations
+        for (userItem, userRating) in userRatings.items():
+            # for every item in our dataset that the user didn't rate
+            for (diffItem, diffRatings) in self.deviations.items():
+                if diffItem not in userRatings and \
+                        userItem in self.deviations[diffItem]:
+                    freq = self.frequencies[diffItem][userItem]
+                    recommendations.setdefault(diffItem, 0.0)
+                    frequencies.setdefault(diffItem, 0)
+                    # add to the running sum representing the numerator
+                    # of the formula
+                    recommendations[diffItem] += (diffRatings[userItem] +
+                                                  userRating) * freq
+                    # keep a running sum of the frequency of diffitem
+                    frequencies[diffItem] += freq
+        recommendations = [(self.convertProductID2name(k),
+                            v / frequencies[k])
+                           for (k, v) in recommendations.items()]
+        # finally sort and return
+        recommendations.sort(key=lambda artistTuple: artistTuple[1],
+                             reverse=True)
+        # I am only going to return the first 50 recommendations
+        return recommendations[:50]
+
+    def pearson(self, rating1, rating2):
+        sum_xy = 0
+        sum_x = 0
+        sum_y = 0
+        sum_x2 = 0
+        sum_y2 = 0
+        n = 0
+        for key in rating1:
+            if key in rating2:
+                n += 1
+                x = rating1[key]
+                y = rating2[key]
+                sum_xy += x * y
+                sum_x += x
+                sum_y += y
+                sum_x2 += pow(x, 2)
+                sum_y2 += pow(y, 2)
+        if n == 0:
+            return 0
+        denominator = sqrt(sum_x2 - pow(sum_x, 2) / n) * \
+                      sqrt(sum_y2 - pow(sum_y, 2) / n)
+        if denominator == 0:
+            return 0
+        else:
+            return (sum_xy - (sum_x * sum_y) / n) / denominator
+
+    def computeNearestNeighbor(self, username):
+        distances = []
+        for instance in self.data:
+            if instance != username:
+                distance = self.fn(self.data[username],
+                                   self.data[instance])
+                distances.append((instance, distance))
+        # sort based on distance -- closest first
+        distances.sort(key=lambda artistTuple: artistTuple[1],
+                       reverse=True)
+        return distances
+
+    def recommend(self, user):
+        recommendations = {}
+        nearest = self.computeNearestNeighbor(user)
+        userRatings = self.data[user]
+        totalDistance = 0.0
+        for i in range(self.k):
+            totalDistance += nearest[i][1]
+        for i in range(self.k):
+            weight = nearest[i][1] / totalDistance
+            name = nearest[i][0]
+            neighborRatings = self.data[name]
+            for artist in neighborRatings:
+                if not artist in userRatings:
+                    if artist not in recommendations:
+                        recommendations[artist] = neighborRatings[artist] * \
+                                                  weight
+                    else:
+                        recommendations[artist] = recommendations[artist] + \
+                                                  neighborRatings[artist] * \
+                                                  weight
+        recommendations = list(recommendations.items())[:self.n]
+        recommendations = [(self.convertProductID2name(k), v)
+                           for (k, v) in recommendations]
+        recommendations.sort(key=lambda artistTuple: artistTuple[1],
+                             reverse=True)
+        return recommendations
+
+r =recommender(users)
+print(r.recommend('Jordyn'))
+print(r.recommend('Hailey'))
