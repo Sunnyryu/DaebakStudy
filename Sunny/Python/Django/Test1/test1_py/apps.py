6b949bc5083ac30bb1347b599f201038a63f217f
sunnyryu <sunny@daebak.co>
2020-03-12 08:57:08 +0900

    django rewind start

--- Sunny/Python/Django/Test1/test1_py/apps.py
+++
@@ -1,5 +0,0 @@
-from django.apps import AppConfig
-
-
-class Test1PyConfig(AppConfig):
-    name = 'test1_py'
---
+++ Sunny/Python/Django/rewind/rewind/apps.py
@@ -0,0 +1,5 @@
+from django.apps import AppConfig
+
+
+class RewindConfig(AppConfig):
+    name = 'rewind'
