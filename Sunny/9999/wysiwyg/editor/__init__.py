5daa7f15f2836efc535dfa420c98cb47ce410f0f
sunny <sunny_ryu@outlook.com>
2020-06-05 16:14:46 +0900

    totorong

--- Sunny/9999/wysiwyg/editor/__init__.py
+++ Sunny/9999/wysiwyg/editor/__init__.py
@@ -5,8 +5,13 @@ import os
 
 load_dotenv(verbose=True)
 secret_key = os.getenv("SECRET_KEY")
+username = os.getenv("USER_NAME")
+password = os.getenv("PASSWORD")
+db_name = os.getenv("DB_NAME")
 app = Flask(__name__)
 app.config['SECRET_KEY'] = secret_key
-app.config['SQLALCHEMY_DATABASE_URI'] = #'mysql://사용자:비밀번호@도메인:3306/서비스이름'
+app.config['SQLALCHEMY_DATABASE_URI'] = f'mysql://{username}:{password}@localhost:3306/{db_name}'
 db = SQLAlchemy(app)
+print(app.config['SQLALCHEMY_DATABASE_URI'])
+
 from editor import routes
--- Sunny/9999/wysiwyg/editor/routes.py
+++ Sunny/9999/wysiwyg/editor/routes.py
@@ -1,8 +1,73 @@
 import os
 from flask import render_template, url_for, Flask, redirect, request
-from editor import app
+from editor import app, db
+from editor.models import Html
+import csv
 
-@app.route("/")
+
+@app.route("/123")
 def index():
+    #with open('C:/Users/tusik/Downloads/BRND-hyunavely-Skirt-Pants-Dress-Belt-71items.csv', encoding='utf-8') as f:
+    with open('C:/Users/tusik/Downloads/BR-ALLCALL-513items.csv', encoding='utf-8') as f:
+        reader = csv.reader(f)
+        next(reader)
+        for i in reader:
+            html = Html(handle = i[0],
+                        title = i[1],
+                        body_HTML = i[2],
+                        vendor = i[3],
+                        type = i[4],
+                        tags = i[5],
+                        published = i[6],
+                        option1_name = i[7], 
+                        option1_value = i[8],
+                        option2_name = i[9],
+                        option2_value = i[10],
+                        option3_name = i[11],
+                        option3_value = i[12],
+                        variant_sku = i[13],
+                        variant_grams = i[14],
+                        variant_inventory_tracker = i[15], 
+                        variant_inventory_qty = i[16],
+                        variant_inventory_policy = i[17],
+                        variant_fullfillment_service = i[18],
+                        varient_price = i[19],
+                        variant_compare_at_price = i[20], 
+                        varient_requires_shipping = i[21],
+                        variant_taxable= i[22],
+                        variant_barcord = i[23],
+                        imgage_src = i[24],
+                        image_position = i[25],
+                        image_alt_text = i[26],
+                        gift_card = i[27],
+                        seo_title= i[28],
+                        seo_description =i[29], 
+                        google_shopping_google_product_category = i[30],
+                        google_shopping_gender = i[31],
+                        goggle_shopping_age_group = i[32],
+                        google_shopping_mpn = i[33],
+                        google_shopping_adwords_grouping = i[34], 
+                        google_shopping_adwords_labels = i[35],
+                        google_shopping_condition = i[36],
+                        google_shopping_custom_product = i[37], 
+                        google_shopping_custion_label0 = i[38],
+                        google_shopping_custion_label1 = i[39],
+                        google_shopping_custion_label2 = i[40],
+                        google_shopping_custion_label3 = i[41],
+                        google_shopping_custion_label4 = i[42],
+                        variant_image = i[43],
+                        variant_weight_unit = i[44],
+                        variant_tax_code = i[45],
+                        cost_per_item = i[46],
+                        )
+            db.create_all()
+            db.session.add(html)
+            db.session.commit()
+            print(1312313312313)
+    
     return render_template('index.html')
 
+
+@app.route("/")
+def home():
+    return render_template('home.html')
